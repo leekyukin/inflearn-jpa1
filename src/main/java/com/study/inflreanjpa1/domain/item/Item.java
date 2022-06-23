4003c463d41c2c71cf33b04c717a87ce8ad47cc0
leekyukin <leekujin14@gmail.com>
2022-06-23 23:57:19 +0900

    ADD :: item entity

---
+++ src/main/java/com/study/inflreanjpa1/domain/item/Item.java
@@ -0,0 +1,30 @@
+package com.study.inflreanjpa1.domain.item;
+
+import com.study.inflreanjpa1.domain.Category;
+import com.study.inflreanjpa1.domain.OrderItem;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype ")
+@Getter @Setter
+public abstract class Item {
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    @OneToMany(mappedBy = "item")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany
+    private List<Category> categories = new ArrayList<>();
+}
