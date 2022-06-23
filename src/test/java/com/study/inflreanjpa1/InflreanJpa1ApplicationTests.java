8d8cc13fcda80cf3c6c9bb79adc88988dc5a8de4
leekyukin <leekujin14@gmail.com>
2022-06-23 23:08:50 +0900

    SET :: initializing

---
+++ src/test/java/com/study/inflreanjpa1/InflreanJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.inflreanjpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflreanJpa1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
