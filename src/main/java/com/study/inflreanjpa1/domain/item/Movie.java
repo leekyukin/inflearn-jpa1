215549262b90d4794fcc217a3f623a5f971f870d
leekyukin <leekujin14@gmail.com>
2022-06-23 23:58:23 +0900

    ADD :: items (book, album, movie)

---
+++ src/main/java/com/study/inflreanjpa1/domain/item/Movie.java
@@ -0,0 +1,13 @@
+package com.study.inflreanjpa1.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("M")
+@Getter @Setter
+public class Movie extends Item {
+}
