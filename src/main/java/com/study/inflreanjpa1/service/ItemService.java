432323f377f5e0aa24a6b8826085dcb93e0fd2e3
leekyukin <leekujin14@gmail.com>
2022-06-25 23:40:04 +0900

    ADD :: ItemService

---
+++ src/main/java/com/study/inflreanjpa1/service/ItemService.java
@@ -0,0 +1,30 @@
+package com.study.inflreanjpa1.service;
+
+import com.study.inflreanjpa1.domain.item.Item;
+import com.study.inflreanjpa1.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+}
