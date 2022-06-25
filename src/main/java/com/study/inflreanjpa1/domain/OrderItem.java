3ba0206f6bdd85dbe4df8216cfca1819c5563ca5
leekyukin <leekujin14@gmail.com>
2022-06-25 23:38:05 +0900

    ADD :: OrderItem

--- src/main/java/com/study/inflreanjpa1/domain/OrderItem.java
+++ src/main/java/com/study/inflreanjpa1/domain/OrderItem.java
@@ -1,15 +1,20 @@
 package com.study.inflreanjpa1.domain;
 
 import com.study.inflreanjpa1.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
-import static javax.persistence.FetchType.*;
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// 생성자로 클레스를 생성하면 오류가 떠서
+// 개발자가 생성자를 사용하지 않는것을 인지하도록 하기 위함
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -24,6 +29,24 @@ public class OrderItem {
     private Order order;
 
     private int orderPrice;
-
     private int count;
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
