6b625c54ee98ff5585c2214463410ac96ff4f9c0
leekyukin <leekujin14@gmail.com>
2022-06-26 14:01:27 +0900

    FIX :: change name

---
+++ src/main/java/com/study/inflreanjpa1/InflearnJpa1Application.java
@@ -0,0 +1,13 @@
+package com.study.inflreanjpa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnJpa1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnJpa1Application.class, args);
+    }
+    
+}
